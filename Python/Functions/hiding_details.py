d45dce403701ef36d1904998d827cdeec55ab841
SHIVANI GUPTA <143402580+Shivani7122@users.noreply.github.com>
2026-05-02 08:17:02 +0000

    "Added files"

---
+++ Python/Functions/hiding_details.py
@@ -0,0 +1,17 @@
+def get_input():
+    print("Getting user input")
+
+def validate_input():
+    print("Validating the input")
+
+def save_to_db():
+    print("Saving the input to Database.")
+
+
+def register_user():
+    get_input()
+    validate_input()
+    save_to_db()
+    print("The user registration Completed!!")
+
+register_user()
