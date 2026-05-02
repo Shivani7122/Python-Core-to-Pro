d1c252bc1f7de47f1461eb5f636a5f15cebbcc5d
SHIVANI GUPTA <143402580+Shivani7122@users.noreply.github.com>
2026-05-02 06:06:25 +0000

    "Added file"

---
+++ Python/Functions/duplication.py
@@ -0,0 +1,5 @@
+def print_order(name , chai_type):    #--- Parameters
+    print(f"{name} ordered {chai_type} chai!")
+
+print_order("Aman","masala")   #--- Arguments
+print_order("Shivani", "Ginger")
