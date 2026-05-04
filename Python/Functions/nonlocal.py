ac8e46d40500de48c5cc10a358b73c048611ecc1
SHIVANI GUPTA <143402580+Shivani7122@users.noreply.github.com>
2026-05-04 04:32:41 +0000

    "file added"

--- Python/Functions/nonlocal.py
+++ Python/Functions/nonlocal.py
@@ -1,7 +1,7 @@
 def update_order():
     chai_type ="Elaichi"
     def kitchen():
-        nonlocal chai_type     #nonlocal means inside to inside function
+        nonlocal chai_type     #nonlocal means inside to inside function, it works on just above the function 
         chai_type = "Kesar"
     kitchen()
     print("After kitchen update",chai_type)
